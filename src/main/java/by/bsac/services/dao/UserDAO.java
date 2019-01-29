33e0a0b162e36f8481b9d8438d41ae903b322d17
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-01-29 10:42:18 +0300

    Data access of User object.

---
+++ src/main/java/by/bsac/services/dao/UserDAO.java
@@ -0,0 +1,21 @@
+package by.bsac.services.dao;
+
+import by.bsac.models.User;
+
+/**
+ * User DAO.
+ * Class which implements this interface must have of all CRUD operations.
+ */
+public interface UserDAO {
+
+    /** Interface methods */
+    User[] findAll();
+    User findByID(Integer a_id);
+    User[] findByFirstName(String a_first_name);
+    User[] findByLastName(String a_last_name);
+    User findByEmail(String a_email);
+    void create();
+    void update();
+    void delete();
+
+}
