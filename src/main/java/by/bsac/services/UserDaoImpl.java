f8bfb3b92ae3af83de95ad519d9d03446b162c7f
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-01-30 11:21:30 +0300

    30.01.2019

--- src/main/java/by/bsac/services/UserDaoImpl.java
+++ src/main/java/by/bsac/services/UserDaoImpl.java
@@ -5,7 +5,7 @@ import by.bsac.services.dao.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
@@ -16,9 +16,9 @@ import java.util.Map;
 
 /**
  *  Class implements User DAO interface.
- *  This is service which have a business logic of CRUD operations on user object in database.
+ *  This is service which have a logic of CRUD operations on user object in database.
  */
-@Service("userDao")
+@Repository("userDao")
 public class UserDaoImpl implements UserDAO {
 
     /* Class global variables */
@@ -44,7 +44,17 @@ public class UserDaoImpl implements UserDAO {
 
     @Override
     public User findByID(Integer a_id) {
-        return null;
+
+        //SQL statement syntax:
+        String sql = "SELECT * FROM USER WHERE USER_ID = :id LIMIT 1;";
+
+        //Map of named parameters to SQL statement:
+        Map<String, Object> named_parameters = new HashMap<>();
+        named_parameters.put("id", a_id);
+
+        //Return User object:
+        return this.je.queryForObject(sql, named_parameters, new UsersRowMapper() );
+
     }
 
     @Override
@@ -77,26 +87,72 @@ public class UserDaoImpl implements UserDAO {
 
     @Override
     public User findByEmail(String a_email) {
-        return null;
+
+        //SQL statement syntax:
+        String sql = "SELECT * FROM USER WHERE USER_ID = :email LIMIT 1;";
+
+        //Map of named parameters to SQL statement:
+        Map<String, Object> named_parameters = new HashMap<>();
+        named_parameters.put("email", a_email);
+
+        //Return User object:
+        return this.je.queryForObject(sql, named_parameters, new UsersRowMapper() );
+
     }
 
     @Override
     public void create(User a_user) {
+
+        //SQL statement syntax:
+        String sql = "INSERT INTO USER(USER_EMAIL, USER_PASSWORD, USER_FNAME, USER_LNAME) " +
+                "VALUES(':email', ':password', ':fname', ':lname');";
+
+        //Map of named parameters to SQL statement:
+        Map<String, Object> named_parameters = new HashMap<>();
+        named_parameters.put("email", a_user.getUserEmail());
+        named_parameters.put("password", a_user.getUserPassword());
+        named_parameters.put("fname", a_user.getFirstName());
+        named_parameters.put("lname", a_user.getLastName());
+
+        //Execute update (INSERT);
+        this.je.update(sql, named_parameters);
 
     }
 
     @Override
     public void update(User a_user) {
 
+        //SQL statement syntax:
+        String sql = " UPDATE USER SET USER_EMAIL = ':email', USER_PASSWORD = ':password', USER_FNAME = ':fname', USER_LNAME = ':lname' WHERE USER_ID = :id LIMIT 1;";
+
+        //Map of named parameters to SQL statement:
+        Map<String, Object> named_parameters = new HashMap<>();
+        named_parameters.put("email", a_user.getUserEmail());
+        named_parameters.put("password", a_user.getUserPassword());
+        named_parameters.put("fname", a_user.getFirstName());
+        named_parameters.put("lname", a_user.getLastName());
+        named_parameters.put("id", a_user.getUserId());
+
+        //Execute update (UPDATE);
+        this.je.update(sql, named_parameters);
     }
 
     @Override
     public void delete(User a_user) {
 
+        //SQL statement syntax:
+        String sql = "DELETE FROM USER WHERE USER_ID = :id LIMIT 1;";
+
+        //Map of named parameters to SQL statement:
+        Map<String, Object> named_parameters = new HashMap<>();
+        named_parameters.put("id", a_user.getUserId());
+
+        //EXECUTE update (DELETE);
+        this.je.update(sql, named_parameters);
     }
 
+    //Custom row mapper.
     private static class UsersRowMapper implements RowMapper<User> {
-
         @Override
         public User mapRow(ResultSet resultSet, int i) throws SQLException {
             User user = new User();
