1d30997fa763e1ee40e03038bf0a73008e858670
SaniukVyacheslav <saniuk.vyacheslav.97@gmail.com>
2019-01-30 09:49:09 +0300

    User DAO Interface

--- src/main/java/by/bsac/services/dao/UserDAO.java
+++ src/main/java/by/bsac/services/dao/UserDAO.java
@@ -10,18 +10,56 @@ import java.util.List;
  */
 public interface UserDAO {
 
-    /** Interface methods */
     /**
-     * Method
-     * @return
+     * Method get data about of all users in database.
+     * @return - list of users.
      */
     List<User> findAll();
+
+    /**
+     * Method search data about user by his ID in database.
+     * @param a_id - User ID.
+     * @return - specific user object.
+     */
     User findByID(Integer a_id);
+
+    /**
+     * Method search data about user by his first name in database.
+     * @param a_first_name - User first name.
+     * @return - specific list of user object.
+     */
     List<User> findByFirstName(String a_first_name);
+
+    /**
+     * Method search data about user by his last name in database.
+     * @param a_last_name - User last name.
+     * @return - specific list of user object.
+     */
     List<User> findByLastName(String a_last_name);
+
+    /**
+     * Method search data about user by his email address in database.
+     * @param a_email - User email address.
+     * @return - specific user object.
+     */
     User findByEmail(String a_email);
+
+    /**
+     * Method write data about specific user in database.
+     * @param a_user - data of user (User object).
+     */
     void create(User a_user);
+
+    /**
+     * Method change data about specific user in database.
+     * @param a_user - data of user (User object).
+     */
     void update(User a_user);
+
+    /**
+     * Method erase data about specific user in database.
+     * @param a_user - data of user (User object).
+     */
     void delete(User a_user);
 
 }
